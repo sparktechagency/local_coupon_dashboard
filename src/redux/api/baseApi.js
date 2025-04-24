5068bd5c99913b4e70f0bf3b0f1415171c31d743
shukumar-542 <shukumar542@gmail.com>
2025-04-24 14:51:16 +0600

    Chnage ip address

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import img from '../../assets/images/place.jpg'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl : 'http://134.199.184.239:3002',
+    baseUrl : 'https://api.impactoapps.com',
     prepareHeaders  :  (headers)=>{
         const token = JSON.parse(localStorage.getItem('coupon_token'));
         if(token){
@@ -17,5 +17,5 @@ export const baseApi = createApi({
     baseQuery : baseQuery,
     endpoints : ()=>({})
 })
-export const imageUrl = "http://134.199.184.239:3002"
+export const imageUrl = "https://api.impactoapps.com"
 export const placeImage = img 
