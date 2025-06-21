6af191fcaf7471c10997753b9f8f289d6d235102
shukumar-542 <shukumar542@gmail.com>
2025-06-21 12:47:14 +0600

    Add ip

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server :{
-    host: "134.199.184.239",
+    // host: "134.199.184.239",
+    host: "0.0.0.0",
     // host: "10.0.60.43",
     port: "3000",
   }
